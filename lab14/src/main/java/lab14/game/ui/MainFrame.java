2643c25698973ba7acc9090b30e59677d2020fc5
Tim <developer.of.gift@gmail.com>
2019-11-10 18:33:31 -0800

    Main Frame class created

---
+++ lab14/src/main/java/lab14/game/ui/MainFrame.java
@@ -0,0 +1,9 @@
+package lab14.game.ui;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame() {
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+    }
+}
