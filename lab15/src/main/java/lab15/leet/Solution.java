5e14067819515f0e42e4db7d9f6f6d1ae812378f
Devel <developer.of.gift@gmail.com>
2019-12-06 21:32:19 -0800

    Dice and config

---
+++ lab15/src/main/java/lab15/leet/Solution.java
@@ -0,0 +1,15 @@
+package lab15.leet;
+
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        int r = 43, f = 33, b = 23;
+        while(r+f+b>1){
+        }
+    }
+    private class group {
+        int val;
+        String name;
+    }
+}
